72d641c035b4f0a51c06a3b9f4dd09339012cf80
tawakalmit <tawakal.mit@gmail.com>
2022-08-10 08:20:46 +0700

    write id join group page

--- pages/joingroup/index.jsx
+++ pages/joingroup/index.jsx
@@ -5,13 +5,13 @@ export default function JoinGroup() {
   return (
     <>
     <div className='w-full h-12 bg-[#1abc9c] flex items-center'>
-        <MdArrowBack className='absolute left-5' size={25} color='white' />
+        <MdArrowBack id='back-to-home' className='absolute left-5' size={25} color='white' />
         <h1 className='text-white text-xl mx-auto'>Join Group</h1>
     </div>
     <form className='w-10/12 mx-auto mt-5'>
-      <input type="text" placeholder=' Enter Group ID' className='w-full bg-[#D9D9D9] h-14 rounded-xl' />
+      <input id='input-groupid' type="text" placeholder=' Enter Group ID' className='w-full bg-[#D9D9D9] h-14 rounded-xl' />
       <div className='flex justify-end'>
-      <button className='w-16 h-7 bg-[#1abc9c] rounded-full text-white mt-3'>Join</button>
+      <button id='btn-join' className='w-16 h-7 bg-[#1abc9c] rounded-full text-white mt-3'>Join</button>
       </div>
     </form>
     </>
