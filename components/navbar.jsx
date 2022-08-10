5fa95b9bfa2172cc5bb677d9a1754214989d0d1e
tawakalmit <tawakal.mit@gmail.com>
2022-08-10 08:14:03 +0700

    write id homepage

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -5,8 +5,8 @@ export default function Navbar() {
   return (
     <div className='w-full h-16 bg-[#1abc9c]'>
         <div className='mx-auto h-16 items-center w-11/12 flex justify-between'>
-            <AiFillHome size={40} color='white' />
-            <AiFillSetting size={40} color='white' />
+            <AiFillHome size={40} color='white' id='btn-home' />
+            <AiFillSetting size={40} color='white' id='btn-setting' />
         </div>
     </div>
   )
