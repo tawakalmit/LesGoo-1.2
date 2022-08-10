c6c40f30b0b4aad0f5d028d9fe4f3a73bfa06db0
syaifulghifari <syaiful.ghifari12@gmail.com>
2022-08-10 15:44:51 +0700

    swagger register

--- pages/register/index.jsx
+++ pages/register/index.jsx
@@ -1,15 +1,64 @@
 import React from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 export default function Register() {
+  const [dataRegister, setRegister] = useState({
+    username: '',
+    email: '',
+    phone: '',
+    password: '',
+  });
+
+  const router = useRouter();
+
+  const handleChangeUserName = (e) => {
+    setRegister((state) => ({ ...state, username: e.target.value }));
+  };
+  const handleChangeEmail = (e) => {
+    setRegister((state) => ({ ...state, email: e.target.value }));
+  };
+  const handleChangePhone = (e) => {
+    setRegister((state) => ({ ...state, phone: e.target.value }));
+  };
+  const handleChangePassword = (e) => {
+    setRegister((state) => ({ ...state, password: e.target.value }));
+  };
+
+  const handleSubmitSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/register`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(dataRegister),
+        }
+      );
+      const data = await response.json();
+      if (response.status === 200) {
+        router.push('/login');
+      } else if (response.status >= 300) {
+        throw data.message;
+      }
+    } catch (error) {
+      console.log('error:', error);
+    }
+  };
+
   return (
     <>
-      <h1 className='text-4xl font-semibold text-center mt-44'>Register</h1>
-      <form className='mt-12 mx-auto w-8/12'>
+      <h1 className='text-4xl font-semibold text-center mt-20'>Register</h1>
+      <form onSubmit={handleSubmitSignUp} className='mt-12 mx-auto w-8/12'>
         <div className='mb-5'>
           <label className='ml-2 font-semibold text-gray-700'>USER NAME</label>
           <input
             id='inputReg-username'
+            onChange={handleChangeUserName}
             type='text'
             placeholder='Joko'
             className='w-full mx-auto p-1 mt-1 rounded-lg pl-2 border-2 font-semibold text-slate-700 border-slate-500 shadow-sm placeholder:text-slate-400 placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-green-400'
@@ -19,6 +68,7 @@ export default function Register() {
           <label className='ml-2 font-semibold text-gray-700'>EMAIL</label>
           <input
             id='inputReg-email'
+            onChange={handleChangeEmail}
             type='email'
             placeholder='joko@lesgoo.com'
             className='w-full mx-auto p-1 mt-1 rounded-lg pl-2 border-2 font-semibold text-slate-700 border-slate-500 shadow-sm placeholder:text-slate-400 placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-green-400'
@@ -28,6 +78,7 @@ export default function Register() {
           <label className='ml-2 font-semibold text-gray-700'>PHONE</label>
           <input
             id='inputReg-phone'
+            onChange={handleChangePhone}
             type='number'
             placeholder='0123456789'
             className='w-full mx-auto p-1 mt-1 rounded-lg pl-2 border-2 font-semibold text-slate-700 border-slate-500 shadow-sm placeholder:text-slate-400 placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-green-400'
@@ -37,36 +88,17 @@ export default function Register() {
           <label className='ml-2 font-semibold text-gray-700'>PASSWORD</label>
           <input
             id='inputReg-password'
+            onChange={handleChangePassword}
             type='Password'
             placeholder='Passwo**'
             className='w-full mx-auto p-1 mt-1 rounded-lg pl-2 border-2 font-semibold text-slate-700 border-slate-500 shadow-sm placeholder:text-slate-400 placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-green-400'
           />
         </div>
-
-        <div className='flex items-center'>
-          <input
-            id='link-checkbox'
-            type='checkbox'
-            value=''
-            required
-            className='w-4 h-4 bg-gray-100 rounded border-gray-300 '
-          />
-          <label
-            htmlFor='link-checkbox'
-            className='ml-2 text-sm font-medium text-gray-700'
-          >
-            I agree with the
-            <span
-              href='#'
-              className='text-green-400 hover:underline cursor-pointer ml-1'
-            >
-              terms and conditions
-            </span>
-            .
-          </label>
-        </div>
-
-        <button id='btn-register' className='my-10 block mx-auto px-5 py-2 rounded-full text-white font-semibold bg-green-400 hover:bg-green-500 active:bg-green-600'>
+        <button
+          id='btn-register'
+          type='submit'
+          className='my-7 block mx-auto px-5 py-2 rounded-full text-white font-semibold bg-green-400 hover:bg-green-500 active:bg-green-600'
+        >
           Sign Up
         </button>
         <div className='flex justify-between text-sm'>
