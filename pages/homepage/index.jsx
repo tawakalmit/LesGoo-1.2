d3b797d1fa480ba15977dbfb24a7b63495832121
tawakalmit <tawakal.mit@gmail.com>
2022-08-10 10:06:24 +0700

    add link homepage

--- pages/homepage/index.jsx
+++ pages/homepage/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Link from 'next/link'
+
 import Navbar from '../../components/navbar'
 import Mygroup from '../../components/mygroup'
 import { MdAddCircle } from 'react-icons/md'
@@ -13,8 +15,12 @@ export default function Homepage() {
             <Mygroup label='Road Trip Rinjani' />
             <Mygroup label='OTW Pelabuhan Ratu' />
         </div>
+        <Link href='/joingroup'>
         <BsPeopleFill className='fixed bottom-5 left-7' size={40} color='#1abc9c' id='btn-joingroup' />
+        </Link>
+        <Link href='/newgroup'>
         <MdAddCircle className='fixed bottom-5 right-5' size={50} color='#1abc9c' id='btn-addgroup' />
+        </Link>
     </div>
   )
 }
