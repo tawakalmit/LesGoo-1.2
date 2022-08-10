6dbcb99761159a62c4678aac70ea914d789d9bc4
tawakalmit <tawakal.mit@gmail.com>
2022-08-10 19:02:01 +0700

    fetch profile and editprofile page

--- pages/editprofile/index.jsx
+++ pages/editprofile/index.jsx
@@ -1,19 +1,112 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdArrowBack } from 'react-icons/md';
 import { CgProfile } from 'react-icons/cg';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Editprofile() {
+
+  const route = useRouter();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect (() => {
+    fetchData();
+  })
+
+  const fetchData = async () => {
+    var requestOptions = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      },
+    };
+    fetch(
+      "https://virtserver.swaggerhub.com/faqihassyfa/LesGoo/1.0.0/users",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        const {data} = result;
+        const {id, username, email, phone} = data;
+        setUsername(username);
+        setEmail(email);
+        setPhone(phone);
+      })
+      .catch((err) => {
+        alert(err.toString())
+      })
+      .finally(() => setLoading(false));
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const body = { username, email, phone };
+
+    let requestOptions = {
+      method: "PUT",
+      headers: {
+        Authorization:
+        { "Content-Type": "application/json" }
+      },
+      body: JSON.stringify(body),
+    };
+
+    fetch(`https://virtserver.swaggerhub.com/faqihassyfa/LesGoo/1.0.0/users`, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        const { message } = result;
+        route.push(`/profile`);
+        alert(message);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error.toString());
+      })
+      .finally();
+  };
+
+  const handleDelete = () => {
+    var requestOptions = {
+      method: "DELETE",
+      headers: {
+        Authorization: { "Content-Type": "application/json" }
+      },
+    };
+
+    fetch("https://virtserver.swaggerhub.com/faqihassyfa/LesGoo/1.0.0/users", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        const { message, code } = result;
+        if (code === 200) {
+          alert(message);
+          route.push("/login");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => setLoading(false));
+  };
+
+
   return (
     <>
       <div>
         <div className='w-full h-12 bg-[#1abc9c] flex items-center'>
-          <Link href='/profile'><MdArrowBack id='back-to-profile' className='absolute left-5' size={25} color='white' /></Link>
+          <MdArrowBack id='back-to-profile' className='absolute left-5' size={25} color='white' onClick={() => {route.push('/profile')}} />
           <h1 className='text-white text-xl mx-auto'>Profile</h1>
         </div>
         <div className='w-10/12 flex flex-col mx-auto items-center'>
           <CgProfile id='edit-image' color='#2c3e50' size={70} className='mt-10' />
-          <form className='mt-12 mx-auto w-8/12'>
+          <form className='mt-12 mx-auto w-8/12' onSubmit={(e) => handleSubmit(e)}>
             <div className='mb-5'>
               <label className='ml-2 font-semibold text-gray-700'>
                 USER NAME
@@ -21,8 +114,9 @@ export default function Editprofile() {
               <input
                 id='edit-username'
                 type='text'
-                placeholder='Joko'
+                placeholder={username}
                 className='w-full mx-auto p-1 mt-1 rounded-lg pl-2 border-2 font-semibold text-slate-700 border-slate-500 shadow-sm placeholder:text-slate-400 placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-green-400'
+                onChange={(e) => setUsername(e.target.value)}
               />
             </div>
             <div className='mb-5'>
@@ -30,8 +124,9 @@ export default function Editprofile() {
               <input
                 id='edit-email'
                 type='email'
-                placeholder='joko@lesgoo.com'
+                placeholder={email}
                 className='w-full mx-auto p-1 mt-1 rounded-lg pl-2 border-2 font-semibold text-slate-700 border-slate-500 shadow-sm placeholder:text-slate-400 placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-green-400'
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className='mb-5'>
@@ -39,21 +134,23 @@ export default function Editprofile() {
               <input
                 id='edit-phone'
                 type='number'
-                placeholder='0123456789'
+                placeholder={phone}
                 className='w-full mx-auto p-1 mt-1 rounded-lg pl-2 border-2 font-semibold text-slate-700 border-slate-500 shadow-sm placeholder:text-slate-400 placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-green-400'
+                onChange={(e) => setPhone(e.target.value)}
               />
             </div>
             <button id='btn-savehanges' className='my-10 block mx-auto px-5 py-2 -mb-6 rounded-full text-white font-semibold bg-green-400 hover:bg-green-500 active:bg-green-600'>
               Save Changes
             </button>
-            <button
-              id='btn-delacc'
-              className='my-10 block mx-auto px-5 py-2 rounded-full text-white font-semibold bg-red-400
-             hover:bg-red-500 active:bg-red-600'
-            >
-              Delete Account
-            </button>
           </form>
+          <button
+          id='btn-delacc'
+          className='my-10 block mx-auto px-5 py-2 rounded-full text-white font-semibold bg-red-400
+          hover:bg-red-500 active:bg-red-600'
+          onClick={() => handleDelete()}
+          >
+          Delete Account
+          </button>
         </div>
       </div>
     </>
--- pages/homepage/index.jsx
+++ pages/homepage/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import Navbar from '../../components/navbar'
 import Mygroup from '../../components/mygroup'
@@ -7,6 +8,9 @@ import { MdAddCircle } from 'react-icons/md'
 import { BsPeopleFill } from 'react-icons/bs'
 
 export default function Homepage() {
+
+  const route = useRouter();
+
   return (
     <div>
         <Navbar />
@@ -15,12 +19,8 @@ export default function Homepage() {
             <Mygroup label='Road Trip Rinjani' />
             <Mygroup label='OTW Pelabuhan Ratu' />
         </div>
-        <Link href='/joingroup'>
-        <BsPeopleFill className='fixed bottom-5 left-7' size={40} color='#1abc9c' id='btn-joingroup' />
-        </Link>
-        <Link href='/newgroup'>
-        <MdAddCircle className='fixed bottom-5 right-5' size={50} color='#1abc9c' id='btn-addgroup' />
-        </Link>
+        <BsPeopleFill  className='fixed bottom-5 left-7' size={40} color='#1abc9c' id='btn-joingroup' onClick={() => {route.push('/joingroup')}} />
+        <MdAddCircle className='fixed bottom-5 right-5' size={50} color='#1abc9c' id='btn-addgroup' onClick={() => {route.push('/newgroup')}} />
     </div>
   )
 }
