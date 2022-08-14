9ad8d9eae35f59a927fcd9b7a25f0534e68640bc
tawakalmit <tawakal.mit@gmail.com>
2022-08-14 16:15:52 +0700

    new group page with maps

--- components/map.jsx
+++ components/map.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, {useRef, useMemo} from 'react'
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
 
-export default function map() {
+export default function map({ center, draggable, onDragMarker, location, popup_label }) {
+
+  const markerRef = useRef(null);
+
+  const dragHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current;
+        if (marker !== null) {
+          onDragMarker(marker.getLatLng());
+        }
+      },
+    }),
+    []
+  );
+
   return (
-    <MapContainer className='w-full h-52' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer className="h-96 w-full" center={[center.lat, center.lng]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-            <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
+        <Marker 
+        position={[
+          location && location.lat ? location.lat : "",
+          location && location.lng ? location.lng : "",
+        ]}
+        draggable={draggable}
+        eventHandlers={dragHandlers}
+        ref={markerRef}
+        >
+            <Popup>{popup_label}</Popup>
         </Marker>
     </MapContainer>
   )
--- pages/newgroup/index.jsx
+++ pages/newgroup/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
 import "leaflet/dist/leaflet.css"
@@ -12,30 +12,111 @@ export default function Newgroup() {
         ssr: false
       });
     const route = useRouter();
+    const [latitude, setLatitude] = useState(null);
+    const [longitude, setLongitude] = useState(null);
+    const [objSubmit, setObjSubmit] = useState("")
+    const [groupname, setGroupname] = useState("");
+    const [groupdes, setGroupdes] = useState("");
+    const [groupimg, setGroupimg] = useState("");
+    const [location, setLocation] = useState({ lat: -6.7169157, lng: 107.0296782});
+    const [destination, setDestination] = useState({ lat: -6.7169157, lng: 107.0296782});
+    const [loading, setLoading] = useState(false)
+    
+    useEffect(()=> {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        });
+      })
+
+      const handleSubmit = async (e) => {
+        alert("group made")
+        setLoading(true);
+        e.preventDefault();
+        const formData = new FormData();
+        for (const key in objSubmit) {
+          formData.append(key, objSubmit[key]);
+        }
+        var requestOptions = {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: formData,
+        };
+    
+        fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/..`, requestOptions)
+          .then((response) => response.json())
+          .then((result) => {
+            alert("message");
+            setObjSubmit({});
+          })
+          .catch((error) => console.log("error", error))
+          .finally(() => setLoading(false));
+      };
+
+      const handleChange = (value, key) => {
+        let temp = { ...objSubmit };
+        temp[key] = value;
+        setObjSubmit(temp);
+      };
+
   return (
     <div>
         <div className='w-full h-12 bg-[#1abc9c] flex items-center'>
             <MdArrowBack id='back-to-home' className='absolute left-5' size={25} color='white' onClick={() => {route.push('/')}} />
             <h1 className='text-white text-xl mx-auto'>New Group</h1>
         </div>
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
             <div className='w-full h-32 bg-white'>
                 <div className='w-10/12 mx-auto flex justify-around'>
                     <CgProfile id='input-groupimage' size={60} color='#2c3e50' className='mt-5' />
-                    <input id='input-groupname' type="text" placeholder=' type group subject' className='mt-5 w-9/12 rounded-xl bg-[#d9d9d9]' />
+                    <input id='input-groupname' type="text" placeholder=' type group subject' className='mt-5 w-9/12 rounded-xl bg-[#d9d9d9]' onChange={(e) => handleChange(e.target.value, "groupname")} />
                 </div>
                 <p className='text-xs text-center mt-3 opacity-50'>Provide a group subject and optional group icon</p>
             </div>
             <div className='w-full mt-10 h-32 bg-white flex flex-col items-center'>
-                <textarea id='input-groupdes' name="description" placeholder=' add group description' cols="30" rows="10" className='rounded-xl mt-5 h-24 bg-[#d9d9d9] w-10/12' ></textarea>
+                <textarea id='input-groupdes' name="description" placeholder=' add group description' cols="30" rows="10" className='rounded-xl mt-5 h-24 bg-[#d9d9d9] w-10/12' onChange={(e) => handleChange(e.target.value, "groupdes")} ></textarea>
             </div>
-            <div className='w-full mt-10 h-32 bg-white'>
+            <div className='w-full mt-10 h-auto bg-white'>
+                <h2 className='p-3'>Start Location</h2>
+                <Map
+                onChange={(e) => handleChange(e.target.value, "location")}
+                popup_label="Start Location"
+                center={location}
+                location={location}
+                draggable={true}
+                onDragMarker={(e) => {
+                    let loc = { lat: e.lat, lng: e.lng };
+                    setLocation(loc);
+                    console.log("start", location)
+                }}
+                />
+                {"lat: " + location.lat}
+                <br />
+                {"lng: " + location.lng}
+            </div>
+            <div className='w-full mt-10 h-auto bg-white'>
                 <h2 className='p-3'>Set Destination</h2>
-                <Map />
+                <Map
+                onChange={(e) => handleChange(e.target.value, "destination")}
+                popup_label="Destination"
+                center={destination}
+                location={destination}
+                draggable={true}
+                onDragMarker={(e) => {
+                    let loc = { lat: e.lat, lng: e.lng };
+                    setDestination(loc);
+                    console.log("finish", destination)
+                }}
+                />
+                {"lat: " + destination.lat}
+                <br />
+                {"lng: " + destination.lng}
             </div>
-            <div className='w-full flex justify-end mt-10'>
+            <button className='w-full flex justify-end mt-10'>
                 <BsFillArrowRightCircleFill id='btn-newgroup' size={40} color='#1abc9c' className='mr-5'/>
-            </div>
+            </button>
         </form>
     </div>
   )
