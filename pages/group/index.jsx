44f9981977d641bf0219d92a6f1b1a9492982786
tawakalmit <tawakal.mit@gmail.com>
2022-08-14 17:51:51 +0700

    add map to group page

--- pages/group/index.jsx
+++ pages/group/index.jsx
@@ -1,10 +1,40 @@
+import { useState, useEffect } from 'react';
 import Navbargroup from '../../components/navbargroup';
 import Footbar from '../../components/footbar';
+import dynamic from 'next/dynamic';
+
 export default function group() {
+
+  const Getmap = dynamic(() => import('../../components/getmap'), {
+    ssr: false
+  });
+
+  const [location, setLocation] = useState({ lat: -6.7169157, lng: 107.0296782});
+  const [destination, setDestination] = useState({ lat: -6.7310985770600755, lng: 107.04116821289064});
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  })
+
   return (
     <>
       <div className='bg-slate-500 '>
         <Navbargroup />
+        <div>
+        <Getmap
+          popup_label="Start Location"
+          center={location}
+          location={location}
+          destination={destination}
+          latitude={latitude}
+          longitude={longitude}
+          />
+        </div>
         <div className='p-2 mb-16'>
           <div className='flex mb-2'>
             <div
