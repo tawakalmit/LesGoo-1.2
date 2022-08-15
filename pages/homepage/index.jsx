e5383fe2e20a83203b3e000a42ffaf8ec59406cd
tawakalmit <tawakal.mit@gmail.com>
2022-08-15 13:17:15 +0700

    revise homepage

--- pages/homepage/index.jsx
+++ pages/homepage/index.jsx
@@ -3,9 +3,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import Navbar from '../../components/navbar';
-import Mygroup from '../../components/mygroup';
-import { MdAddCircle } from 'react-icons/md';
-import { BsPeopleFill } from 'react-icons/bs';
+import Image from 'next/image';
 
 export default function Homepage() {
   const route = useRouter();
@@ -13,29 +11,18 @@ export default function Homepage() {
   return (
     <div>
       <Navbar />
-      <h1 className='text-xl mt-5 font-bold text-center'>My Groups</h1>
-      <div className='mt-5'>
-        <Mygroup label='Road Trip Rinjani' />
-        <Mygroup label='OTW Pelabuhan Ratu' />
+      <div onClick={(e) => route.push('/newgroup')} className='flex items-center justify-center w-[330px] h-auto bg-[#1abc9c] mx-auto mt-5 rounded-lg cursor-pointer'>
+        <div className='w-auto h-auto p-4 flex flex-col'>
+        <Image src='/1.jpg' width={300} height={200} />
+        <h1 className='text-center text-2xl mt-3 text-white'>New Group</h1>
+        </div>
       </div>
-      <BsPeopleFill
-        className='fixed bottom-5 left-7'
-        size={40}
-        color='#1abc9c'
-        id='btn-joingroup'
-        onClick={() => {
-          route.push('/joingroup');
-        }}
-      />
-      <MdAddCircle
-        className='fixed bottom-5 right-5'
-        size={50}
-        color='#1abc9c'
-        id='btn-addgroup'
-        onClick={() => {
-          route.push('/newgroup');
-        }}
-      />
+      <div onClick={(e) => route.push('/joingroup')} className='mb-20 flex items-center justify-center w-[330px] h-auto bg-[#1abc9c] mx-auto mt-5 rounded-lg cursor-pointer'>
+        <div className='w-auto h-auto p-4 flex flex-col'>
+        <Image src='/2.jpg' width={300} height={200} />
+        <h1 className='text-center text-2xl mt-3 text-white'>Join Group</h1>
+        </div>
+      </div>
     </div>
   );
 }
